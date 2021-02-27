548020c5ade8e3da2cd727fc38d2ba4e78781cc8
william-shelly <william-shelly@users.noreply.github.com>
2021-02-26 18:05:39 -0600

    Updating styles and content

--- src/components/mainnav.js
+++ src/components/mainnav.js
@@ -1,11 +1,11 @@
 export default function MainNav() {
     return (
         <ul className="main-nav text-center text-lg-left">
-            <li><a href="#top" className="text-danger">Home</a></li>
-            <li><a href="#about" className="text-danger">About</a></li>
-            <li><a href="#menu" className="text-danger">Menu</a></li>
-            <li><a href="#contact" className="text-danger">Contact</a></li>
-            <li><a href="/checkout" className="text-danger">Checkout</a></li>
+            <li><a href="#top" className="">Home</a></li>
+            <li><a href="#about" className="">About</a></li>
+            <li><a href="#menu" className="">Menu</a></li>
+            <li><a href="#contact" className="">Contact</a></li>
+            <li><a href="/checkout" className="">Checkout</a></li>
         </ul>
     )
 }
