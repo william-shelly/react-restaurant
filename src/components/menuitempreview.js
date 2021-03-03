9cbafe031fd004fb6b7e5af4ed1fcbc02856e95e
william-shelly <william-shelly@users.noreply.github.com>
2021-03-02 19:16:39 -0600

    Adding quotes to loading=lazy

--- src/components/menuitempreview.js
+++ src/components/menuitempreview.js
@@ -1,7 +1,7 @@
 export default function MenuItemPreview(props) {
     return (
         <>
-            {props.image ? <div className="menu-item-img col-12 col-lg-3 mb-4"><img src={props.image} alt={props.name} loading=lazy /></div> : undefined }
+            {props.image ? <div className="menu-item-img col-12 col-lg-3 mb-4"><img src={props.image} alt={props.name} loading="lazy" /></div> : undefined }
             <div className="menu-item-description col-12 col-lg-3 mb-4 text-center text-lg-left">
                 <h2>{props.name}</h2>
                 <p><span dangerouslySetInnerHTML={{ __html: props.description}}></span></p>
